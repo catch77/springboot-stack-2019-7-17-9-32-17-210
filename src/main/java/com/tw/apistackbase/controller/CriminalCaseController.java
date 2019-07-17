86395ec36b360d089bfdbb8da0e53d7054909c15
catch77 <1297056296@qq.com>
2019-07-17 18:33:07 +0800

    ADD: finish Story1 AC2

--- src/main/java/com/tw/apistackbase/controller/CriminalCaseController.java
+++ src/main/java/com/tw/apistackbase/controller/CriminalCaseController.java
@@ -1,14 +1,23 @@
 package com.tw.apistackbase.controller;
 
+import com.tw.apistackbase.model.CriminalCase;
 import com.tw.apistackbase.repo.CriminalCaseRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/criminalcases")
 public class CriminalCaseController {
 
     @Autowired
     private CriminalCaseRepo caseRepo;
 
+    @GetMapping("{id}")
+    public CriminalCase find(@PathVariable String id) {
+        return caseRepo.findById(id).orElse(null);
+    }
 
 }
