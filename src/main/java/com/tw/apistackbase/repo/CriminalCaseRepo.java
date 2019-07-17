573c0070c5c0a2bfdf8473c79d1ff438dd713911
catch77 <1297056296@qq.com>
2019-07-17 18:44:57 +0800

    ADD: finish Story1 AC4

--- src/main/java/com/tw/apistackbase/repo/CriminalCaseRepo.java
+++ src/main/java/com/tw/apistackbase/repo/CriminalCaseRepo.java
@@ -3,12 +3,16 @@ package com.tw.apistackbase.repo;
 import com.tw.apistackbase.model.CriminalCase;
 import org.hibernate.annotations.OrderBy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Set;
 
 public interface CriminalCaseRepo extends JpaRepository<CriminalCase, String> {
 
     @OrderBy(clause = "happenTime DESC")
     List<CriminalCase> findAll();
 
+    @Query
+    Set<CriminalCase> findByName();
 }
